d81e3b73960f0c40253587208b2746959fba88af
ozaki25 <fs65ugumori@yahoo.co.jp>
2019-05-09 12:39:47 +0900

    add comment

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 $(function() {
+  // variables
   var $title = $('.title');
   var $showButton = $('.show-button');
   var $hideButton = $('.hide-button');
 
+  // listeners
   $showButton.on('click', onClickShow);
   $hideButton.on('click', onClickHide);
 
+  // functions
   function onClickShow(e) {
     e.preventDefault();
     $title.show();
