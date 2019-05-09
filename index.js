63b7f2a2ed6f1fdede214a0aad113a2bccc8a8c2
ozaki <fs65ugumori@yahoo.co.jp>
2019-05-09 23:26:33 +0900

    add navigation

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ $(function() {
   // variables
   var $title = $('.title');
   var $toggleTitleButton = $('.toggle-title-button');
+  var $nextButton = $('.next-button');
+  var $sections = $('.section');
+  var position = 0;
 
   // listeners
   $toggleTitleButton.on('change', onChange);
+  $nextButton.on('click', onClickNext);
 
   // functions
   function onChange(e) {
@@ -13,4 +17,18 @@ $(function() {
     if ($radio.val() === '0') $title.show();
     if ($radio.val() === '1') $title.hide();
   }
+
+  function onClickNext(e) {
+    e.preventDefault();
+    position += 1;
+    render();
+  }
+
+  function render() {
+    $sections.hide();
+    $('.section-' + (position % 3)).show();
+  }
+
+  // executes
+  render();
 });
