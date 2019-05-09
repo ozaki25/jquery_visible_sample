0273a52662d54d42a74ca3590b37255444c65963
ozaki <fs65ugumori@yahoo.co.jp>
2019-05-09 23:42:34 +0900

    add bug sample

---
+++ multi_page_bug/index.js
@@ -0,0 +1,23 @@
+$(function() {
+  // variables
+  var $title = $('.title');
+  var $toggleTitleButton = $('.toggle-title-button');
+  var $sections = $('.section');
+
+  // listeners
+  $toggleTitleButton.on('change', onChange);
+
+  // functions
+  function onChange(e) {
+    e.preventDefault();
+    var $radio = $(e.target);
+
+    if ($radio.val() === '0') {
+      $title.show();
+    }
+
+    if ($radio.val() === '1') {
+      $title.hide();
+    }
+  }
+});
---
+++ single_page_bug/index.js
@@ -0,0 +1,40 @@
+$(function() {
+  // variables
+  var $title = $('.title');
+  var $toggleTitleButton = $('.toggle-title-button');
+  var $nextButton = $('.next-button');
+  var $sections = $('.section');
+  var position = 0;
+
+  // listeners
+  $toggleTitleButton.on('change', onChange);
+  $nextButton.on('click', onClickNext);
+
+  // functions
+  function onChange(e) {
+    e.preventDefault();
+    var $radio = $(e.target);
+
+    if ($radio.val() === '0') {
+      $title.show();
+    }
+
+    if ($radio.val() === '1') {
+      $title.hide();
+    }
+  }
+
+  function onClickNext(e) {
+    e.preventDefault();
+    position += 1;
+    render();
+  }
+
+  function render() {
+    $sections.hide();
+    $('.section-' + (position % 3)).show();
+  }
+
+  // executes
+  render();
+});
